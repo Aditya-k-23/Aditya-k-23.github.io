3e3a66e0f26dccd528cdfddcf90caf4e814ee066
Aditya Kulkarni <65531635+Aditya-k-23@users.noreply.github.com>
2025-03-19 11:32:15 -0400

    fix: update baseUrl variable type

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 
-let baseUrl = "https://adityakulkarni.me/";
+const baseUrl = "https://adityakulkarni.me/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
